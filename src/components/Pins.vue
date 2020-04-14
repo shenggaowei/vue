0fb23fea8805235c8c50700adedc5e2a0f44d096
weishenggao <weishenggao@innobuddy.com>
2020-04-14 19:19:50 +0800

    init

---
+++ src/components/Pins.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="pins-container">
+    <a class="tag" target="_blank" href="http://www.17sucai.com/">item1</a>
+    <a class="tag" target="_blank" href="http://www.17sucai.com/">item2</a>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+.tagBall{width:800px;height:800px;margin:50px auto;position:relative;}
+.tag{display:block;position:absolute;left:0px;top:0px;color:#000;text-decoration:none;font-size:15px;font-family:"微软雅黑";font-weight:bold;}
+.tag:hover{border:1px solid #666;}
+</style>
